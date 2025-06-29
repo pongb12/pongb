58cd035ed36a610167c50b43f2c056eb8495497e
pongb12 <st163943@gmail.com>
2025-06-29 23:40:38 +0700

    steal-a-brainrot.lua

--- steal-a-brainrot.lua
+++ steal-a-brainrot.lua
@@ -87,7 +87,7 @@ local activeFeatures = {}
 local gameId = 109983668079237
 
 -- GUI Size Settings
-local originalGUISize = UDim2.new(0, 220, 0, 230)
+local originalGUISize = UDim2.new(0, 180, 0, 230)
 local maximizedGUISize = UDim2.new(0, 350, 0, 320)
 local minimizedGUISize = UDim2.new(0, 150, 0, 30)
 
