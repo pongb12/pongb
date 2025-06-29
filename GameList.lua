d3ccfbf3d636d475530b265b13e23aec74046739
pongb12 <st163943@gmail.com>
2025-06-29 20:29:16 +0700

    GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,4 +1,4 @@
 local Games = {
-  [109983668079237] = "https://raw.githubusercontent.com/pongb12/pongb/refs/heads/main/GameList.lua", --Steal a Brainrot
+  [109983668079237] = "https://raw.githubusercontent.com/pongb12/pongb/refs/heads/main/steal-a-brainrot.lua", --Steal a Brainrot
 
 return Games
