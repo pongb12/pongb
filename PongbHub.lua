c6c81c42675d6d5f374c8cd3d908c74f8f577f22
pongb12 <st163943@gmail.com>
2025-10-13 22:10:55 +0700

    Delete PongbHub.lua

--- PongbHub.lua
+++
@@ -1,7 +0,0 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/pongb12/pongb/refs/heads/main/GameList.lua"))()
-
-for PlaceID, Execute in pairs(Games) do
-    if PlaceID == game.PlaceId then
-        loadstring(game:HttpGet(Execute))()
-    end
-end
